ab61d7ee989cea71d716946c6545ef132eb169f0
= <=>
2023-02-19 02:01:50 +0100

    fixed dates

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
 export const getYear = (date: string) => {
+  if(!date) return;
   return new Date(date).getFullYear()
+}
+
+export const getMonthAndYear = (date: string) => {
+  if(!date) return;
+  const month = new Date(date).getMonth() + 1
+  const year = new Date(date).getFullYear()
+  return `${month.toString().padStart(2, '0')}/${year}`
+}
+
+export const getFullDate = (date: string) => {
+  if(!date) return;
+  const day = new Date(date).getDate()
+  const month = new Date(date).getMonth() + 1
+  const year = new Date(date).getFullYear()
+  return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`
 }
